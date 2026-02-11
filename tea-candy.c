2aedae6c36000f8f116fa55c7785ddc1d5914fa1
Dayyalaramya <ramyadayyala8@gmail.com>
2026-02-11 22:51:11 +0530

    Add files via upload

---
+++ tea-candy.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int tea;
+int candy;
+printf("enter amount of tea:\n");
+scanf("%d",&tea);
+printf("enter amount of candy:\n");
+scanf("%d",&candy);
+if(tea<5 || candy<5)
+{
+printf("0");
+}
+else if(tea>=2*candy || candy>=2*tea)
+{ 
+printf("2");
+}
+else
+ {
+printf("1");}
+return 0;
+}
