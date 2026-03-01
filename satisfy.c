ef10b461490719a1eefdefbfe896f4a61b2f468d
Dayyalaramya <ramyadayyala8@gmail.com>
2026-03-01 17:37:00 +0530

    Add files via upload

---
+++ satisfy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("enter the number:");
+    scanf("%d",&num);
+    if(num>=10)
+    {
+    printf("satisfy the condition");
+    }
+    else
+    {
+    printf("not satisfy the condition");
+    }
+
+    return 0;
+}
